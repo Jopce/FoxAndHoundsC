1fb76f40276374b5c7691c47f1896479e5f27fc0
Jopce <Jokubass.Petraitis@gmail.com>
2023-12-13 14:55:57 +0200

    fix misplaced files

---
+++ src/boardDisplay.h
@@ -0,0 +1,18 @@
+    //Corner symbols
+    char cornerChar[4] = {201, 187, 200, 188};
+    char cornerCharV2[4] = {218, 191, 192, 217};
+
+    //Row and column symbols
+    char lineChar[2] = {205, 186}; // 0 - row and 1 - column
+    char lineCharV2[2] = {196, 179}; // 0 - row and 1 - column
+
+    //Side connection symbols
+    char sideChar[2] = {185, 204}; //other values - 180, 195
+
+    //Top and bottom connection symbols
+    char TnBChar[2] = {203, 202}; //0 - connection from top to bottom, 1 - connection from bottom to top
+    //other values - 194, 193
+
+    //All connection symbol
+    char conAllChar = 206;
+    char conAllCharV2 = 197;
