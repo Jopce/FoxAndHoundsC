c7ec4ee22693a94beece40ff1ebae843bd1fa8f6
StarcClans <47033137+StarcClans@users.noreply.github.com>
2023-12-20 19:21:12 +0200

    Update files_logic.h
    yeah

--- src/files_logic.h
+++ src/files_logic.h
@@ -1,5 +1,9 @@
 #define n 8
 
+void processInput(int *save, int *array2, int *array3, int *rowW, int *colW, int *player);
+
+void displayLastModifiedTime();
+
 void check_lose(int **board, int *row, int *col, int *win, int *lose);
 
 void generateRowAndColNames(int choice, int *rowValue, int *colValue, int *array2, int *array3);
@@ -8,10 +12,10 @@ void NewgenerateRowAndColNames(int choice, int *rowValue, int *colValue, int *ar
 
 int getUserInput();
 
-void mover(int **board, int *row, int *col, int direction);
+void mover(int **board, int *row, int *col, int direction, int *player);
 
 void printBoard(int **board);
 
-void pawnsMove(int **board, int *row, int *col, int direction);
+void pawnsMove(int **board, int *row, int *col, int direction, int *player);
 
 int logic();
