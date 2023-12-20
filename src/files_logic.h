57544123d8998a406a1392f71c04c975c8a8b316
StarcClans <47033137+StarcClans@users.noreply.github.com>
2023-12-20 19:17:36 +0200

    Update files_logic.h
    changed it a bit to fit the required format.

--- src/files_logic.h
+++ src/files_logic.h
@@ -1,6 +1,3 @@
-#include<stdio.h>
-#include<stdlib.h>
-
 #define n 8
 
 void check_lose(int **board, int *row, int *col, int *win, int *lose);
@@ -18,28 +15,3 @@ void printBoard(int **board);
 void pawnsMove(int **board, int *row, int *col, int direction);
 
 int logic();
-
-int rowValue = 0;
-int colValue = 0;
-
-int userChoice;
-int col1 = 7;
-int col2 = 5;
-int col3 = 3;
-int col4 = 1;
-int row1=0;
-int row2=0;
-int row3=0;
-int row4=0;
-
-int rowW = 7; // Initial position for 'F'
-int colW = 4;
-
-int number = 2; //priority value
-
-int **board = (int **)calloc(n, sizeof(int *));
-
-int win = 0;
-int lose = 0;
-
-int move;
