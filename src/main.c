cd7f606cc6067b2e3dab653e24a00868fdab7174
Cryptic5 <84860494+Cryptic5@users.noreply.github.com>
2023-12-20 21:41:12 +0200

    Update main.c
    Pakeiciau i savo funkcijas. Dar reiketu keliu duomenu, kad normaliai veiktu.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "opponents.h"
 #include "file_logic.h"
+#include "mainMenuDisplay.h"
+#include "boardDisplay.h"
 // importint .h failus
 
 int main()
@@ -15,11 +17,11 @@ int main()
                                               {0, 0, 0, 0, 0, 0, 0, 0},
                                               {F, 0, 0, 0, 0, 0, 0, 0}};
 
-    int game_select = display_menu(); // igno kazkokia funkcija
+    int game_select = menuCLI();; // igno kazkokia funkcija
 
     if (game_select == 1)
     {
-        int difficulty = get_difficulty(); // igno kazkokia funkcija
+        int difficulty = menuCLI(); // igno kazkokia funkcija
 
         switch (difficulty)
         {
@@ -52,17 +54,19 @@ int main()
     }
     else
     {
-        exit_game(); // igno funkcija kuri isjungia programa
+        exitGame(); // igno funkcija kuri isjungia programa
         return 0;
     }
 
     do
-    {
-        int new_move = display_board(game_board); // igno funkcija kuri grazina skaiciuka kur paejo Fox
+    {                   //Reikia informacijos -: VS_Player: 1 if playing VS another player, else 0 | foxOrHoundsTurn: 1 if fox, else 0 hounds
+        int new_move = displayBoard (board[8][8], VS_Player, difficulty, foxOrHoundsTurn); // igno funkcija kuri grazina skaiciuka kur paejo Fox
 
         make_a_move(&game_board, new_move); // luko funkcija kuri grazina nauja boarda su padarytu ejimu
 
     } while (!luko_game_over_function()); // luko funkcija kuri tikrina ar game over
 
-    display_result(game_board); // igno funkcija kuri paziuri kas laimejo ir isveda rezultata
+    //display_result(game_board); /// igno funkcija kuri paziuri kas laimejo ir isveda rezultata
+    ///Reikia informacijos -: PlayerVSwho: 1 - player, 0 - bots | playerWinVSbot: 1 - win, 0 - lose | playerWinVSplayer: 1 - fox win, 0 - hounds win <--------- need variables
+    results (points, PlayerVSwho, playerWinVSbot, playerWinVSplayer);
 }
