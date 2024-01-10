6eacbbb1aa0838df8b5a34e6817145ff69a29b28
StarcClans <47033137+StarcClans@users.noreply.github.com>
2024-01-10 23:02:36 +0200

    Update files_logic.h

--- src/files_logic.h
+++ src/files_logic.h
@@ -2,15 +2,15 @@
 
 typedef int** Board;
 
-void Pwin_lose(int *win, int *lose, int *number);
+void Pwin_lose(int *win, int *lose);
 
 void freeMemory(Board board, int *array2, int *array3);
 
 Board allocateBoard();
 
-void save_game(int *array2,int *array3, int *rowW, int *colW, int *player);
+void save_game(int *array2,int *array3, int *rowW, int *colW, int *player,int *difficulty, Board board);
 
-void processInput(int *save, int *array2, int *array3, int *rowW, int *colW, int *player);
+void processInput(int *save, int *array2, int *array3, int *rowW, int *colW, int *player, int *difficulty, Board board);
 
 void displayLastModifiedTime();
 
@@ -22,8 +22,12 @@ void NewgenerateRowAndColNames(int choice, int *rowValue, int *colValue, int *ar
 
 int getUserInput();
 
-void fMove(Board board, int *row, int *col, int direction, int *player, int *difficulty);
+void fMove(Board board, int *row, int *col, int direction);
 
 void printBoard(Board board);
 
-void hMove(Board board, int *row, int *col, int direction, int *player, int *difficulty);
+void hMove(Board board, int *row, int *col, int direction);
+
+void boardTransfer(Board board, game_board[n][n]);
+
+void transferBoard(Board board, game_board[n][n]);
